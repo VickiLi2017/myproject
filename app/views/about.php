2645fcec26cf359e4aae977269e903d41f4b9af5
VickiLi2017 <vl54@njit.edu>
2017-09-28 13:03:21 -0400

    create project

---
+++ app/views/about.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>Welcome to About! Page is under construction1</h1>
+        --@yield('content')
+    </body>
+</html>
---
+++ app/views/services.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>Wellcome to services page!</h1>
+        --@yield('content')
+    </body>
+</html>
